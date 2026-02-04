51759926897e4e5bb159e94c54cef33caabce5fb
Michal Konkel <konkel.michal@gmail.com>
2026-02-04 23:53:55 +0100

    adding spotless and detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,30 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.spotless)
+    alias(libs.plugins.detekt)
+}
+
+spotless {
+    kotlin {
+        target("**/*.kt")
+        targetExclude("**/build/**/*.kt")
+        ktlint()
+    }
+    kotlinGradle {
+        target("**/*.kts")
+        targetExclude("**/build/**/*.kts")
+        ktlint()
+    }
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    allRules = false
+    config.setFrom("$rootDir/config/detekt/detekt.yml")
+    parallel = true
+}
+
+dependencies {
+    detektPlugins(libs.detekt.compose.rules)
 }
