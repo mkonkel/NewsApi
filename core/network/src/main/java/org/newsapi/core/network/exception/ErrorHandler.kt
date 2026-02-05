2f747aa40056efa8b9676e40e3115b11fa8457fe
Michal Konkel <konkel.michal@gmail.com>
2026-02-05 15:59:06 +0100

    api error handling

---
+++ core/network/src/main/java/org/newsapi/core/network/exception/ErrorHandler.kt
@@ -0,0 +1,31 @@
+package org.newsapi.core.network.exception
+
+import retrofit2.HttpException
+import java.io.IOException
+import java.net.SocketTimeoutException
+
+object ErrorHandler {
+    fun handle(throwable: Throwable): NetworkException =
+        when (throwable) {
+            is HttpException -> {
+                when (throwable.code()) {
+                    401 -> UnauthorizedException("API key is invalid or missing")
+                    in (400..499) -> NetworkErrorException("Client error: ${throwable.code()}")
+                    in (500..599) -> ServerException(throwable.code(), "Server error: ${throwable.code()}")
+                    else -> UnknownException("Unexpected HTTP error: ${throwable.code()}")
+                }
+            }
+
+            is SocketTimeoutException -> {
+                NetworkErrorException("Request timeout")
+            }
+
+            is IOException -> {
+                NetworkErrorException("Network error: ${throwable.message}")
+            }
+
+            else -> {
+                UnknownException("Unknown error: ${throwable.message}")
+            }
+        }
+}
