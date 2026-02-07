f261a7b8cdb3fe1c018bbbbe01dd0f2019ba5f1e
Michal Konkel <konkel.michal@gmail.com>
2026-02-07 20:08:53 +0100

    Shape

---
+++ core/ui/src/main/java/org/newsapi/core/ui/theme/Shape.kt
@@ -0,0 +1,12 @@
+package org.newsapi.core.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val AppShapes =
+    Shapes(
+        small = RoundedCornerShape(4.dp),
+        medium = RoundedCornerShape(8.dp),
+        large = RoundedCornerShape(16.dp),
+    )
