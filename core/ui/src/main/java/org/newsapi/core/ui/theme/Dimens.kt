29b5b4ea2822fb16f6f07c0ae2a78723d4de1118
Michal Konkel <konkel.michal@gmail.com>
2026-02-07 20:08:26 +0100

    Dimens

---
+++ core/ui/src/main/java/org/newsapi/core/ui/theme/Dimens.kt
@@ -0,0 +1,15 @@
+package org.newsapi.core.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val PaddingSmall = 8.dp
+    val PaddingMedium = 16.dp
+    val PaddingLarge = 24.dp
+    val SpacingSmall = 8.dp
+    val SpacingMedium = 16.dp
+    val SpacingLarge = 24.dp
+    val CardElevation = 4.dp
+    val FabSize = 56.dp
+    val FabMargin = 16.dp
+}
